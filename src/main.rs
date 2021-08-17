3ab37d826d032fc199132a42546552499d9a2060
HEKUCHAN <Heitorhirose@gmail.com>
2021-08-17 14:48:15 +0900

    add database and  post methods

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
-use thiserror::Error;
+use actix_web::{get, http::header, post, web, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::params;
+use serde::Deserialize;
+use thiserror::Error;
+
+#[derive(Deserialize)]
+struct AddParams {
+    text: String,
+}
+
+#[derive(Deserialize)]
+struct DeleteParams {
+    id: u32,
+}
 
 struct TodoEntry {
     id: u32,
@@ -10,7 +24,7 @@ struct TodoEntry {
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate {
-    entries: Vec<TodoEntry>,    
+    entries: Vec<TodoEntry>,
 }
 
 // エラーをまとめる enum を定義する
@@ -19,25 +33,56 @@ struct IndexTemplate {
 enum MyError {
     #[error("Failed to render HTML")]
     AskamaError(#[from] askama::Error),
+
+    #[error("Failed to get connection")]
+    ConnectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error)
 }
 
 // actix_web::ResponseError を MyError に実装する
 // 今回はデフォルトの実装をそのまま使うから、新たに実装するものはない
 impl ResponseError for MyError {}
 
+#[post("/add")]
+async fn add_todo(
+    params: web::Form<AddParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
+#[post("/delete")]
+async fn delete_todo(
+    params: web::Form<DeleteParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("DELETE FROM todo WHERE id=?", &[&params.id])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
 #[get("/")]
-async fn index() -> Result<HttpResponse, MyError> {
+async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    let mut statement = conn.prepare("SELECT id, text FROM todo")?;
+    let rows = statement.query_map(params![], |row| {
+        let id = row.get(0)?;
+        let text = row.get(1)?;
+        Ok(TodoEntry { id , text })
+    })?;
+
     let mut entries = Vec::new();
-
-    entries.push(TodoEntry {
-        id: 1,
-        text: "First entry".to_string(),
-    });
-
-    entries.push(TodoEntry {
-        id: 2,
-        text: "Second entry".to_string(),
-    });
+    for row in rows {
+        entries.push(row?);
+    }
 
     let html = IndexTemplate { entries };
     let response_body = html.render()?;
@@ -52,9 +97,30 @@ async fn index() -> Result<HttpResponse, MyError> {
 
 #[actix_web::main]
 async fn main() -> Result<(), actix_web::Error> {
-    HttpServer::new(move || App::new().service(index))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await?;
+    let manager = SqliteConnectionManager::file("todo.db");
+    let pool = Pool::new(manager)
+        .expect("Failed to initialize the connection poll.");
+    let conn = pool
+        .get()
+        .expect("Failed to get the connection from the pool.");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todo (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL
+        )",
+        params![],
+    )
+    .expect("FAILED to create a table `todo`.");
+
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .service(add_todo)
+            .service(delete_todo)
+            .data(pool.clone())
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await?;
     Ok(())
 }
